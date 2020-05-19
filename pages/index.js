46db6b8b3d5a4a0ae992d7e3fe74bbb6e1a0ebbd
Nirav Patel <niravkpatel28@gmail.com>
2020-05-19 23:55:29 +0530

    on Click navigation with router hook implemented

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,29 @@
 import Axios from "axios";
 import Layout from "../components/layout";
 import styles from "./index.module.css";
+import { useRouter } from "next/router";
+import { route } from "next/dist/next-server/server/router";
 const Index = (props) => {
   //   console.log(props);
+
+  const router = useRouter();
+  const navigateBlog = (event) => {
+    router.push(`/blogs/${event.target.id}`);
+  };
+  console.log(router);
   return (
     <Layout>
       <div className={styles.blogList}>
         <h1> Blog List </h1>
         {props.blogs.map((blog) => {
           return (
-            <p id={blog.id} key={blog.id}>
+            <p
+              id={blog.id}
+              key={blog.id}
+              onClick={(event) => {
+                navigateBlog(event);
+              }}
+            >
               {" "}
               {blog.title}{" "}
             </p>
