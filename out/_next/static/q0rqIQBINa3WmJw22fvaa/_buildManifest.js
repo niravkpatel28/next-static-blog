48bb9619f49df066938c0a929cc0a5ac01fc56cb
Nirav Patel <niravkpatel28@gmail.com>
2020-05-19 23:41:39 +0530

    Pre rendered blog pages exported

---
+++ out/_next/static/q0rqIQBINa3WmJw22fvaa/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/blogs/[id]":["static\u002Fcss\u002F929cd6a00010c84158f2.css"],"/index":[a]}}("static\u002Fcss\u002Fefe4f8052477230a99ff.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
